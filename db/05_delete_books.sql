0a228c7b97412016dea7dbad3b78211fe5bc0567
neil-hughes <neil.hughes@techreturners.com>
2024-02-13 11:36:23 +0000

    feat: Initial commit

---
+++ db/05_delete_books.sql
@@ -0,0 +1,3 @@
+\c my_bookshop
+
+\echo '\n These books have been removed from the data as they are out of stock:\n'
---
+++ db/16_query_books_with_genres.sql
@@ -0,0 +1,7 @@
+\c my_bookshop
+
+\echo '\n Here are the genres for Emma:\n'
+
+
+
+\echo '\n Here are all the dystopian books:\n'
