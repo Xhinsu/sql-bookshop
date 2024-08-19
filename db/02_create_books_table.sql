dc1289476bd81ff5bc124043a57aa12a8d5a0193
Jez <jezdavisuk@gmail.com>
2024-08-19 11:52:28 +0100

    Add: Task two

--- db/02_create_books_table.sql
+++ db/02_create_books_table.sql
@@ -1,3 +1,11 @@
 \c my_bookshop;
 
 -- create the books table here
+CREATE TABLE books(
+	book_id SERIAL PRIMARY KEY, 
+	book_title VARCHAR(40) NOT NULL,
+        price_in_pence INT,
+	quantity_in_stock INT,
+	release_date INT,
+	is_fiction BOOLEAN	
+);
